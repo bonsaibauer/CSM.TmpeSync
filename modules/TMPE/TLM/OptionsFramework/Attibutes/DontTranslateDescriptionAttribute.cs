c0e5f48bcf9e56fd11c41e192bddc4e3a37f889b
Philipp <mail@philippfeierabend.de>
2025-10-23 19:31:13 +0200

    Add TMPE source as normal files (no submodule)

---
+++ modules/TMPE/TLM/OptionsFramework/Attibutes/DontTranslateDescriptionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OptionsFramework.Attibutes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class DontTranslateDescriptionAttribute : DescriptionAttribute
+    {
+        public DontTranslateDescriptionAttribute(string description)
+            : base(description) {
+        }
+    }
+}
