9568e5243f5d367191bb5243cdcd5ad1e00a89b8
Philipp <mail@philippfeierabend.de>
2025-10-24 18:51:27 +0200

    Squashed 'subtrees/CSM/' content from commit ca338f21
    git-subtree-dir: subtrees/CSM
    git-subtree-split: ca338f21cf1830b550e4b7167a24f2de340200ae

---
+++ src/api/Networking/Player.cs
@@ -0,0 +1,23 @@
+﻿using CSM.API.Networking.Status;
+
+namespace CSM.API.Networking
+{
+    public class Player
+    {
+        public string Username { get; set; }
+
+        public long Latency { get; set; }
+
+        public ClientStatus Status { get; set; }
+
+        public Player(string username)
+        {
+            Username = username;
+            Latency = -1;
+        }
+
+        public Player()
+        {
+        }
+    }
+}
