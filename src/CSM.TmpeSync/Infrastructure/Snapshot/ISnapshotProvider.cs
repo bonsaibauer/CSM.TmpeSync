b40f6a19cb58f07a4bfa15eb8660bc934cf7f6ce
Philipp <mail@philippfeierabend.de>
2025-10-25 19:08:02 +0200

    Revert "Inline TM:PE infrastructure into each feature"

--- src/CSM.TmpeSync/Infrastructure/Snapshot/ISnapshotProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace CSM.TmpeSync.Snapshot
-{
-    public interface ISnapshotProvider
-    {
-        void Export(); // Host sendet Applied-Events
-        void Import(); // (optional)
-    }
-}
