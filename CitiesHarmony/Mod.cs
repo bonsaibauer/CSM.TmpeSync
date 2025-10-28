8172b94de62adb7611c82a3d30182476a2dc2ef8
Philipp <mail@philippfeierabend.de>
2025-10-28 14:47:53 +0100

    Squashed 'subtrees/CitiesHarmony/' content from commit b5b6563d
    git-subtree-dir: subtrees/CitiesHarmony
    git-subtree-split: b5b6563d5d52cacbee47271c2fb9e1862609059f

---
+++ CitiesHarmony/Mod.cs
@@ -0,0 +1,23 @@
+﻿using ICities;
+
+namespace CitiesHarmony
+{
+    public class Mod : IUserMod
+    {
+        public string Name => $"Harmony 2.2.2-0";
+        public string Description => "Mod Dependency";
+
+        static Mod()
+        {
+            // Try to patch all Harmony 1 assemblies before they are used for patching
+            // (this is possible due to a change in 1.15.1-f4 that constructs all mods first before calling OnEnabled on any of them)
+            Installer.Run();
+        }
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            _ = new Settings(helper);
+        }
+        
+    }
+}
